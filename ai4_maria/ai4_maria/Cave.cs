533346fb0e99391f87d0aad2e90263a6be9a71d5
Maria Luque Anguita <mluqueanguita@gmail.com>
2018-03-09 01:37:43 +0000

    first comit

---
+++ ai4_maria/ai4_maria/Cave.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ai4_maria
+{
+    class Cave
+    {
+
+        private int _x;
+        private int _y;
+        private int _caveID;
+
+        private double _g;
+        private double _h;
+
+        private Cave _fromCave;
+        private List<Cave> _toCavesList = new List<Cave>();
+
+        public Cave(int x, int y, int caveID)
+        {
+            _x = x;
+            _y = y;
+            _caveID = caveID;
+        }
+        // x, y and caveID
+        public int getX
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public int getY
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public int CaveID
+        {
+            get { return _caveID; }
+            set { _caveID = value; }
+        }
+
+        // G value
+        public double getG_value
+        {
+            get { return _g; }
+        }
+
+        public double calculateGCostTo(Cave c)
+        {
+            double distance = System.Math.Sqrt((this._x * c._x) + (this._y * c._y));
+            return (c._g + distance);
+        }
+
+        public void setGCostTo(Cave c)
+        {
+            double distance = System.Math.Sqrt((this._x * c._x) + (this._y * c._y));
+            this._g = (c._g + distance);
+        }
+
+        // H value
+        public void setHeuristicCostToGoal(Cave c)
+        {
+            double distance = System.Math.Sqrt((this._x * c._x) + (this._y * c._y));
+            this._h = distance;
+        }
+
+        // Total cost
+        public double getTotalCost()
+        {
+            return (_g + _h);
+        }
+
+        // From cave
+        public Cave FromCave
+        {
+            set { _fromCave = value; }
+            get { return _fromCave; }
+        }
+
+        // To caves
+        public void addToCavesList(Cave c)
+        {
+            this._toCavesList.Add(c);
+        }
+
+        public List<Cave> ToCavesList
+        {
+            get { return _toCavesList; }
+        }
+
+        // ToString
+        public string toString()
+        {
+            return "Cave { x = " + _x + 
+                ", y = " + _y + 
+                ", caveID = " + _caveID + "}";
+        }
+
+        // Equals
+        public Boolean Equals(Cave c)
+        {
+            if (c == null)
+            {
+                return false;
+            }
+
+            if (this._x == c._x && this._y == c._y && this._caveID == c._caveID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ai4_maria/ai4_maria/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ai4_maria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Cave> caves = new List<Cave>();
+            int caveID = 1;
+
+
+            //Read input file
+            string text = System.IO.File.ReadAllText(@"C:\Users\Maria\Documents\2nd year\Artificial Intelligence\try2\caverns files\input2.cav");
+            Console.WriteLine("Raw data: " + text);
+
+            //convert data to an array
+            String[] data = text.Split(',');
+            Console.WriteLine(data[0] + ' ' + data[1]);
+
+            //extract data from the array
+            int noOfCaves = Int16.Parse(data[0]);
+            Console.WriteLine("There are " + noOfCaves + " caves.");
+
+            //Get coordinates and build caves
+            for (int count = 1; count < ((noOfCaves * 2) + 1); count = count + 2)
+            {
+                Console.WriteLine("Cave at " + data[count] + ","+ data[count + 1]);
+                int x = Int16.Parse(data[count]);
+                int y = Int16.Parse(data[count + 1]);
+
+                Cave cave = new Cave(x, y, caveID);
+                caves.Add(cave);
+
+                caveID++;
+            }
+
+            //print all caves
+            Console.WriteLine("caves list:");
+            foreach(Cave c in caves)
+            {
+                Console.WriteLine(c.toString());
+            }
+
+
+            //line 101 in cavParses of darwon
+            //boolean[][] connectedMatrix = this.buildConnectivityMatrix(noOfCaves, data);
+
+
+
+
+
+            //Build connectivity matrix
+
+            //Declare the array
+
+            Boolean[][] connected = new Boolean[noOfCaves][];
+
+            for(int _row = 0; _row < noOfCaves; _row++)
+            {
+                connected[_row] = new Boolean[noOfCaves];
+            }
+            //Now read data, starting point in array is after the coordinates
+            int col = 0; int row = 0;
+            for (int point = (noOfCaves*2)+1; point < data.Length; point++)
+            {
+                //work through the array
+
+                if (data[point].Equals("1"))
+                    connected[row][col] = true;
+                else
+                    connected[row][col] = false;
+
+                row++;
+                if (row == noOfCaves)
+                {
+                    row = 0;
+                    col++;
+                }
+            }
+
+            //find if caves are connected with caveIDs
+            int cave1 = 11;
+            int cave2 = 12;
+
+            Boolean areConnected = connected[cave1 - 1][cave2 - 1];
+            Console.WriteLine("Cave " + cave1 + " and cave " + cave2 + " are connected: " + areConnected);
+
+            
+            //Generate toCavesList
+            for (int i = 1; i < caves.Count() + 1; i++)
+            {
+                Cave tempCave = null;
+                Cave possibleChild = null;
+
+                foreach(Cave c in caves)
+                {
+                    if (c.CaveID == i)
+                    {
+                        tempCave = c;
+                        Console.WriteLine("tempCave: "+tempCave.toString());
+                    }
+                }
+
+                for(int j = 1; j < caves.Count(); j++)
+                {
+                    foreach (Cave d in caves)
+                    {
+                        if (d.CaveID == i)
+                        {
+                            possibleChild = d;
+                            //Console.WriteLine("3");
+                        }
+                    }
+
+                    int cave1ID = tempCave.CaveID;
+                    int cave2ID = possibleChild.CaveID;
+                    //Console.WriteLine("4");
+
+                    Boolean check = connected[cave1ID - 1][cave2ID - 1];
+                    //Console.WriteLine("hi: "+check);
+
+                    Boolean yes = true;
+                    //Console.WriteLine(".");
+
+                    if (!tempCave.Equals(possibleChild) && yes)
+                    {
+                        tempCave.addToCavesList(possibleChild);
+                        Console.WriteLine("it works");
+                    }
+                }
+
+
+                //print caves to list
+                foreach(Cave a in tempCave.ToCavesList)
+                {
+                    Console.WriteLine("Cave "+tempCave.CaveID+" can go to caves: "+ a.CaveID);
+                }
+                
+            }
+
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
