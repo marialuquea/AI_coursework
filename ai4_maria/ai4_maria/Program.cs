49ec574dd11ae6dcdf34475b748f4c6f02cf03e4
Maria Luque Anguita <mluqueanguita@gmail.com>
2018-03-14 11:44:29 +0000

    if file not find

--- ai4_maria/ai4_maria/Program.cs
+++ ai4_maria/ai4_maria/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.IO;
 using System.Reflection;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace ai4_maria
 {
@@ -12,7 +11,7 @@ namespace ai4_maria
         public static void Main(string[] args)
         {
 
-            Boolean flag = setBooleanFlag();
+            Boolean flag = true;
 
             while (flag == true) {
 
@@ -23,115 +22,101 @@ namespace ai4_maria
                 //string text = System.IO.File.ReadAllText(@"C:\Users\Maria\Documents\2nd year\Artificial Intelligence\try2\AI_coursework\ai4_maria\input2.cav");
                 string text = GetCavesFiles();
 
-                //convert data to an array
-                String[] data = text.Split(',');
-
-                //extract data from the array
-                int noOfCaves = Int16.Parse(data[0]);
-
-                //Get coordinates and build caves
-                for (int count = 1; count < ((noOfCaves * 2) + 1); count = count + 2)
+                if (text != null)
                 {
-                    int x = Int16.Parse(data[count]);
-                    int y = Int16.Parse(data[count + 1]);
+                    //convert data to an array
+                    String[] data = text.Split(',');
 
-                    Cave cave = new Cave(x, y, caveID);
-                    caves.Add(cave);
+                    //extract data from the array
+                    int noOfCaves = Int16.Parse(data[0]);
 
-                    caveID++;
-                }
-
-                //Build connectivity matrix
-                //Declare the array
-
-                Boolean[][] connected = new Boolean[noOfCaves][];
-
-                for (int _row = 0; _row < noOfCaves; _row++)
-                {
-                    connected[_row] = new Boolean[noOfCaves];
-                }
-                //Now read data, starting point in array is after the coordinates
-                int col = 0; int row = 0;
-                for (int point = (noOfCaves * 2) + 1; point < data.Length; point++)
-                {
-                    //work through the array
-
-                    if (data[point].Equals("1"))
-                        connected[row][col] = true;
-                    else
-                        connected[row][col] = false;
-
-                    row++;
-                    if (row == noOfCaves)
+                    //Get coordinates and build caves
+                    for (int count = 1; count < ((noOfCaves * 2) + 1); count = count + 2)
                     {
-                        row = 0;
-                        col++;
+                        int x = Int16.Parse(data[count]);
+                        int y = Int16.Parse(data[count + 1]);
+
+                        Cave cave = new Cave(x, y, caveID);
+                        caves.Add(cave);
+
+                        caveID++;
                     }
-                }
 
-                //Generate toCavesList
-                for (int i = 1; i < caves.Count + 1; i++)
-                {
-                    Cave tempCave = null;
-                    Cave possibleChild = null;
+                    //Build connectivity matrix
+                    //Declare the array
 
-                    foreach (Cave c in caves)
+                    Boolean[][] connected = new Boolean[noOfCaves][];
+
+                    for (int _row = 0; _row < noOfCaves; _row++)
                     {
-                        if (c.CaveID == i)
+                        connected[_row] = new Boolean[noOfCaves];
+                    }
+                    //Now read data, starting point in array is after the coordinates
+                    int col = 0; int row = 0;
+                    for (int point = (noOfCaves * 2) + 1; point < data.Length; point++)
+                    {
+                        //work through the array
+
+                        if (data[point].Equals("1"))
+                            connected[row][col] = true;
+                        else
+                            connected[row][col] = false;
+
+                        row++;
+                        if (row == noOfCaves)
                         {
-                            tempCave = c;
+                            row = 0;
+                            col++;
                         }
                     }
 
-                    for (int j = 1; j < caves.Count + 1; j++)
+                    //Generate toCavesList
+                    for (int i = 1; i < caves.Count + 1; i++)
                     {
-                        foreach (Cave d in caves)
+                        Cave tempCave = null;
+                        Cave possibleChild = null;
+
+                        foreach (Cave c in caves)
                         {
-                            if (d.CaveID == j)
+                            if (c.CaveID == i)
                             {
-                                possibleChild = d;
+                                tempCave = c;
                             }
                         }
 
-                        int cave1ID = tempCave.CaveID;
-                        int cave2ID = possibleChild.CaveID;
-
-                        Boolean check = connected[cave1ID - 1][cave2ID - 1];
-
-                        if (!tempCave.Equals(possibleChild) && check)
+                        for (int j = 1; j < caves.Count + 1; j++)
                         {
-                            tempCave.addToCavesList(possibleChild);
+                            foreach (Cave d in caves)
+                            {
+                                if (d.CaveID == j)
+                                {
+                                    possibleChild = d;
+                                }
+                            }
+
+                            int cave1ID = tempCave.CaveID;
+                            int cave2ID = possibleChild.CaveID;
+
+                            Boolean check = connected[cave1ID - 1][cave2ID - 1];
+
+                            if (!tempCave.Equals(possibleChild) && check)
+                            {
+                                tempCave.addToCavesList(possibleChild);
+                            }
                         }
                     }
+
+                    //Call A* function
+                    Cave startCave = (Cave)caves[0];
+                    Cave endCave = (Cave)caves[caves.Count - 1];
+
+                    ArrayList aStarCaves = AstarSearch(startCave, endCave);
                 }
-
-                //Call A* function
-                Cave startCave = (Cave)caves[0];
-                Cave endCave = (Cave)caves[caves.Count - 1];
-
-                ArrayList aStarCaves = AstarSearch(startCave, endCave);
-
-              
-
-
-
+                else
+                {
+                    Console.ReadKey();
+                }
             }
-
-            //flag = false;
-
-            //Console.ReadKey();
-            //Console.Write("Do you want to check another file? (yes/no): ");
-            /*
-            string answer = Console.ReadLine();
-            if (answer == "yes")
-            {
-
-            }
-            */
-
-
-
-
         }
 
         //A star algorithm
@@ -209,35 +194,31 @@ namespace ai4_maria
             return solutionPath;
         }
 
+        //Read file that user types
         static string GetCavesFiles()
         {
 
-            //current directory
-            //string path = Directory.GetCurrentDirectory();
             string path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            //Console.WriteLine(path);
-
-
-            string newPath = Path.GetFullPath(Path.Combine(path, @"..\..\..\"));
-            //Console.WriteLine(newPath);
 
             Console.Write("Name of file you want to check (include extension): ");
             string input = Console.ReadLine();
 
-            string caverns_path = Path.GetFullPath(Path.Combine(path, @"..\..\..\")) + input;
-            string files = System.IO.File.ReadAllText(caverns_path);
 
+            try
+            {
+                string caverns_path = Path.GetFullPath(Path.Combine(path, @"..\..\..\")) + input;
 
+                string files = System.IO.File.ReadAllText(caverns_path);
 
-            string maria = System.IO.File.ReadAllText(@"C:\Users\Maria\Documents\2nd year\Artificial Intelligence\try2\AI_coursework\ai4_maria\input2.cav");
-            return files;
-            
+                string maria = System.IO.File.ReadAllText(@"C:\Users\Maria\Documents\2nd year\Artificial Intelligence\try2\AI_coursework\ai4_maria\input2.cav");
+                return files;
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("File not found... Press enter to try again");
+                return null;
+            }
         }
-
-
-        static Boolean setBooleanFlag()
-        {
-            return true;
-        }
+        
     }
 }
